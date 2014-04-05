bf013c2831b3d06399ab64127cfcc429afb0c3a8
Una Kravets <Una21136@yahoo.com>
2014-04-05 01:56:13 -0400

    Craft setup and integration

---
+++ craft/app/fieldtypes/DateFieldType.php
@@ -0,0 +1,216 @@
+<?php
+namespace Craft;
+
+/**
+ * Craft by Pixel & Tonic
+ *
+ * @package   Craft
+ * @author    Pixel & Tonic, Inc.
+ * @copyright Copyright (c) 2014, Pixel & Tonic, Inc.
+ * @license   http://buildwithcraft.com/license Craft License Agreement
+ * @link      http://buildwithcraft.com
+ */
+
+/**
+ *
+ */
+class DateFieldType extends BaseFieldType
+{
+	/**
+	 * Returns the type of field this is.
+	 *
+	 * @return string
+	 */
+	public function getName()
+	{
+		return Craft::t('Date/Time');
+	}
+
+	/**
+	 * Returns the content attribute config.
+	 *
+	 * @return mixed
+	 */
+	public function defineContentAttribute()
+	{
+		return AttributeType::DateTime;
+	}
+
+	/**
+	 * Defines the settings.
+	 *
+	 * @access protected
+	 * @return array
+	 */
+	protected function defineSettings()
+	{
+		return array(
+			'showDate' => array(AttributeType::Bool, 'default' => true),
+			'showTime' => AttributeType::Bool,
+		);
+	}
+
+	/**
+	 * Returns the field's settings HTML.
+	 *
+	 * @return string|null
+	 */
+	public function getSettingsHtml()
+	{
+		// If they are both selected or nothing is selected, the select showBoth.
+		if (($this->getSettings()->showDate && $this->getSettings()->showTime))
+		{
+			$value = 'showBoth';
+		}
+		else if ($this->getSettings()->showDate)
+		{
+			$value = 'showDate';
+		}
+		else if ($this->getSettings()->showTime)
+		{
+			$value = 'showTime';
+		}
+
+		return craft()->templates->renderMacro('_includes/forms.html', 'radioGroupField', array(array(
+			'id' => 'dateTime',
+			'name' => 'dateTime',
+			'options' => array(
+				array(
+					'label' => Craft::t('Show date'),
+					'value' => 'showDate',
+				),
+				array(
+					'label' => Craft::t('Show time'),
+					'value' => 'showTime',
+				),
+				array(
+					'label' => Craft::t('Show date and time'),
+					'value' => 'showBoth',
+				)
+			),
+			'value' => $value,
+		)));
+	}
+
+	/**
+	 * Returns the field's input HTML.
+	 *
+	 * @param string $name
+	 * @param mixed  $value
+	 * @return string
+	 */
+	public function getInputHtml($name, $value)
+	{
+		$variables = array(
+			'id'       => craft()->templates->formatInputId($name),
+			'name'     => $name,
+			'value'    => $value
+		);
+
+		$input = '';
+
+		// In case nothing is selected, default to the date.
+		if (!$this->getSettings()->showDate && !$this->getSettings()->showTime)
+		{
+			$this->getSettings()->showDate = true;
+		}
+
+		if ($this->getSettings()->showDate)
+		{
+			$input .= craft()->templates->render('_includes/forms/date', $variables);
+		}
+
+		if ($this->getSettings()->showTime)
+		{
+			$input .= ' '.craft()->templates->render('_includes/forms/time', $variables);
+		}
+
+		return $input;
+	}
+
+	/**
+	 * Returns the input value as it should be saved to the database.
+	 *
+	 * @param mixed $value
+	 * @return mixed
+	 */
+	public function prepValueFromPost($value)
+	{
+		if ($value)
+		{
+			// Ugly? Yes. Yes it is.
+			$timeString = $value->format(DateTime::MYSQL_DATETIME, DateTime::UTC);
+			return DateTime::createFromFormat(DateTime::MYSQL_DATETIME, $timeString, craft()->getTimeZone());
+		}
+	}
+
+	/**
+	 * Convert back to the server's timezone.
+	 *
+	 * @param mixed $value
+	 * @return DateTime
+	 */
+	public function prepValue($value)
+	{
+		if ($value)
+		{
+			return $value->setTimezone(new \DateTimeZone(craft()->getTimeZone()));
+		}
+	}
+
+	/**
+	 * Modifies an element query that's filtering by this field.
+	 *
+	 * @param DbCommand $query
+	 * @param mixed     $value
+	 * @return null|false
+	 */
+	public function modifyElementsQuery(DbCommand $query, $value)
+	{
+		if ($value !== null)
+		{
+			$handle = $this->model->handle;
+			$query->andWhere(DbHelper::parseDateParam('content.'.craft()->content->fieldColumnPrefix.$handle, $value, $query->params));
+		}
+	}
+
+	/**
+	 * @param array $settings
+	 * @return array
+	 */
+	public function prepSettings($settings)
+	{
+		if (isset($settings['dateTime']))
+		{
+			switch ($settings['dateTime'])
+			{
+				case 'showBoth':
+				{
+					unset($settings['dateTime']);
+					$settings['showTime'] = true;
+					$settings['showDate'] = true;
+
+					break;
+				}
+				case 'showDate':
+				{
+					unset($settings['dateTime']);
+					$settings['showDate'] = true;
+					$settings['showTime'] = false;
+
+					break;
+				}
+				case 'showTime':
+				{
+					unset($settings['dateTime']);
+					$settings['showTime'] = true;
+					$settings['showDate'] = false;
+
+					break;
+				}
+			}
+		}
+
+		return $settings;
+	}
+}
---
+++ craft/app/migrations/m140401_000003_tag_groups.php
@@ -0,0 +1,77 @@
+<?php
+namespace Craft;
+
+/**
+ * Craft by Pixel & Tonic
+ *
+ * @package   Craft
+ * @author    Pixel & Tonic, Inc.
+ * @copyright Copyright (c) 2014, Pixel & Tonic, Inc.
+ * @license   http://buildwithcraft.com/license Craft License Agreement
+ * @link      http://buildwithcraft.com
+ */
+
+/**
+ * The class name is the UTC timestamp in the format of mYYMMDD_HHMMSS_migrationName
+ */
+class m140401_000003_tag_groups extends BaseMigration
+{
+	/**
+	 * Any migration code in here is wrapped inside of a transaction.
+	 *
+	 * @return bool
+	 */
+	public function safeUp()
+	{
+		MigrationHelper::refresh();
+
+		if (craft()->db->tableExists('tagsets'))
+		{
+			// A couple people have had failed updates that resulted in tagsets *and* taggroups tables lying around
+			// causing a MySQL error if trying to rename the tagsets table
+			// so let's make sure it's gone first.
+			if (craft()->db->tableExists('taggroups'))
+			{
+				$this->dropTable('taggroups');
+
+				// ...and refresh the schema cache
+				craft()->db->getSchema()->refresh();
+			}
+
+			Craft::log('Renaming the tagsets table to taggroups.', LogLevel::Info, true);
+			MigrationHelper::renameTable('tagsets', 'taggroups');
+		}
+
+		if (craft()->db->columnExists('tags', 'setId'))
+		{
+			Craft::log('Renaming the tags.setId column to groupId.', LogLevel::Info, true);
+			MigrationHelper::renameColumn('tags', 'setId', 'groupId');
+		}
+
+		Craft::log('Updating the Tags fields\' settings.', LogLevel::Info, true);
+
+		$fields = craft()->db->createCommand()
+			->select('id, settings')
+			->from('fields')
+			->where('type="Tags"')
+			->queryAll();
+
+		foreach ($fields as $field)
+		{
+			$settings = JsonHelper::decode($field['settings']);
+
+			if (isset($settings['source']) && strncmp($settings['source'], 'tagset:', 7) === 0)
+			{
+				$settings['source'] = 'taggroup:'.substr($settings['source'], 7);
+
+				$this->update('fields', array(
+					'settings' => JsonHelper::encode($settings)
+				), array(
+					'id' => $field['id']
+				));
+			}
+		}
+
+		return true;
+	}
+}
