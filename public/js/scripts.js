d2157c0ed595272941e69cba9aa64e08fe632517
Una Kravets <Una21136@yahoo.com>
2014-04-18 00:50:50 -0400

    adding interactivity

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,18 +1,30 @@
 window.SGIF || (SGIF = {});
+
 (function($, window, document, undefined) {
+
   var _common = SGIF.common = { //this is your js object
     init: function() {
     this.setVars();
     this.bindEvents();
   },
+
   setVars: function() {
     // set all variables here
     this.helloString = "hello" ; // also _common.helloString
   },
+
   bindEvents: function() {
-    // set event listeners for your functions here
+    $(document).ready(function(){ console.log('doc loaded');});
+    $('.ham-menu').on('click', this.menuOpen);
   },
 
+  // Menu Opening
+  menuOpen: function() {
+    console.log('clicked');
+    $('.sidebar, .shader').toggleClass('opened');
+  },
+  
+//  Gifs play on click or hover. Replace with static image if they don't
   gifPlay: $(function() {
     $('img').each(function(e){
      var src = $(e).attr('src');
@@ -23,7 +35,7 @@ window.SGIF || (SGIF = {});
         });
       });
     }),
-  // the rest of your functions go here
-  _common.init(); // don't for
-	}
-});
+
+   };
+  $(document).ready(function(){SGIF.common.init();}); // don't for
+})(jQuery, window, document);
